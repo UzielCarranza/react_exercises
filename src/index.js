fc3191eb4812f993191e9c53a60f073300f176da
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-08 15:22:09 -0400

    practiced with array destructuring

--- src/index.js
+++ src/index.js
@@ -5,7 +5,33 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App authorized={true} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App authorized={true}/>
+    </React.StrictMode>
 );
+
+
+//   --------------------- ARRAY DESTRUCTURING
+
+
+//
+
+function App2() {
+
+    const [, , light] = [
+        "boots",
+        "tent",
+        "headlamp",
+
+    ]
+}
+
+const root2 = ReactDOM.createRoot(document.getElementById('array-destructuring'));
+root2.render(
+    <React.StrictMode>
+        <App2/>
+    </React.StrictMode>
+);
+
+
+
