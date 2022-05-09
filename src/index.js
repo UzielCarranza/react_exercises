78fa33f71e1c041d72b420274188da7bb01f4cd8
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-08 22:06:04 -0400

    practiced with loading states, displaying data from API'S, AND fetching data with hooks

--- src/index.js
+++ src/index.js
@@ -52,22 +52,35 @@ function App3() {
 // https://api.github.com/users/UzielCarranza
 function App4({login}) {
     const [data, setData] = useState(null);
-
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
     useEffect(() => {
+        if (!login) return;
+        setLoading(true);
         fetch(`https://api.github.com/users/${login}`)
             .then(res => res.json())
-            .then(setData);
+            .then(setData)
+            .then(() => setLoading(false))
+            .catch(setError);
     }, []);
 
-    if (data) {
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+    if (error) {
+        return <pre>{JSON.stringify(error, null, 2)} < /pre>
+    }
+
+    if (!data){
+        return null;
+    }
+
         return (
             <div>
                 <h1>{data.name}</h1>
                 <img src={data.avatar_url} alt={data.login} width={200} height={200}/>
             </div>
         )
-    }
-
 
 }
 
@@ -87,7 +100,7 @@ root3.render(
     </React.StrictMode>
 );
 
-const root4 = ReactDOM.createRoot(document.getElementById('use--effect'));
+const root4 = ReactDOM.createRoot(document.getElementById('assynchronous-react'));
 root4.render(
     <React.StrictMode>
         <App4 login={"UzielCarranza"}/>
