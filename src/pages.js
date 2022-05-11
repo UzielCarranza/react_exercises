ac28446278e7cb4688168e80d253f8a93de1c184
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-11 09:41:00 -0400

    created components to display information as wr navigagte the website

---
+++ src/pages.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+
+
+export function Home(){
+    return (
+        <div>
+            <h1>[Company website]</h1>
+        </div>
+    )
+}
+export function About(){
+    return (
+        <div>
+            <h1>[About page]</h1>
+        </div>
+    )
+}
+export function Events(){
+    return (
+        <div>
+            <h1>[Events]</h1>
+        </div>
+    )
+}
+export function Contacts(){
+    return (
+        <div>
+            <h1>[Contacts]</h1>
+        </div>
+    )
+}
