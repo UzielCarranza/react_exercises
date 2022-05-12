8e28c66e9165fe99b298f5773301dd21a7c967ac
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-11 21:11:27 -0400

    added links using routers

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import {Routes, BrowserRouter as Router, Route} from "react-router-dom";
 import {Home, About, Contacts, Events} from "./pages";
 
+import {Link} from "react-router-dom";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
--- src/pages.js
+++ src/pages.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 
 
@@ -7,6 +8,13 @@ export function Home(){
     return (
         <div>
             <h1>[Company website]</h1>
+            <nav>
+                <Link to="about">about</Link>
+
+                <Link to="events">events</Link>
+
+                <Link to="contact">contact</Link>
+            </nav>
         </div>
     )
 }
@@ -14,6 +22,14 @@ export function About(){
     return (
         <div>
             <h1>[About page]</h1>
+            <nav>
+                <Link to="home">Home</Link>
+                <Link to="about">About</Link>
+
+                <Link to="events">Events</Link>
+
+                <Link to="contacts">Contact</Link>
+            </nav>
         </div>
     )
 }
@@ -21,6 +37,14 @@ export function Events(){
     return (
         <div>
             <h1>[Events]</h1>
+            <nav>
+                <Link to="about">Home</Link>
+                <Link to="about">About</Link>
+
+                <Link to="events">Events</Link>
+
+                <Link to="contacts">Contact</Link>
+            </nav>
         </div>
     )
 }
@@ -28,6 +52,14 @@ export function Contacts(){
     return (
         <div>
             <h1>[Contacts]</h1>
+            <nav>
+                <Link to="home">Home</Link>
+                <Link to="about">About</Link>
+
+                <Link to="events">Events</Link>
+
+                <Link to="contacts">Contacts</Link>
+            </nav>
         </div>
     )
 }
